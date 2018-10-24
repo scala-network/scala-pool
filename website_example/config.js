33fee0e49eecfb1d7e27e5df590d4e0405524107
Azizul Hakimi Mohd Yussuf Izzudin <cryptoamity@gmail.com>
2018-10-24 21:43:11 +0800

    Force refresh after block submit

--- website_example/config.js
+++ website_example/config.js
@@ -1,1 +1,9 @@
-var api="https://xtlpool.com";var poolHost="stratum.xtlpool.com";var email="support@xtlpool.com";var telegram="";var discord="https://discord.gg/zng9k2D";var marketCurrencies=["{symbol}-BTC","{symbol}-USD","{symbol}-EUR","{symbol}-CAD"];var blockchainExplorer="https://explorer.stellite.cash/search?value={id}";var transactionExplorer="https://explorer.stellite.cash/search?value={id}";var themeCss="themes/stellite.css";
+var api="http://testnet.xtlpool.com:8118";
+var poolHost="testnet.xtlpool.com";
+var email="support@xtlpool.com";
+var telegram="";
+var discord="https://discord.gg/zng9k2D";
+var marketCurrencies=["{symbol}-BTC","{symbol}-USD","{symbol}-EUR","{symbol}-CAD"];
+var blockchainExplorer="https://explorer.stellite.cash/search?value={id}";
+var transactionExplorer="https://explorer.stellite.cash/search?value={id}";
+var themeCss="themes/stellite.css";
