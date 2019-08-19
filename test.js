3b3f43e75470ed6fd9c5a2fe1b01a75b04762db7
root <root@s143784.wholesaleinternet.net>
2019-08-19 11:58:38 -0500

    Cleaning up for parent repo with defy only for now

--- test.js
+++
@@ -1,9 +0,0 @@
-const args = require("args-parser")(process.argv);
-
-global.config = require('./lib/bootstrap')(args.config || 'config.json');
-require('./lib/logger.js');
-
-
-global.required = (lib) =>{
-	return require('.'+lib);
-}
