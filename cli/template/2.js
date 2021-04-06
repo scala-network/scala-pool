81677e52c48f7d9af69ea1f7a7a2c24b6bd9566c
ahmyi <cryptoamity@gmail.com>
2021-04-06 23:09:57 +0800

    WIP: Add some redirect options

--- cli/template/2.js
+++ cli/template/2.js
@@ -10,7 +10,40 @@ module.exports = cli => {
 	2) On Mining \n\
 	3) Both\n");
 
-	const ans1 = cli.questionSync("Where do you want miners from events to redirect?");
-	const ans2 = cli.questionSync("Which miners do you want to filter for redirect?");
-	
+	const parse = cli.parseInt(ans)
+	if(parse === false || parse === null) {
+		cli.quit();
+		return;
+	}
+
+
+
+	const ans1 = cli.questionSync("Where do you want miners from event to redirect?");
+	const ans2 = cli.questionSync("Which miners do you want to filter for redirect?\n\
+1) ALL\n\
+2) Continent\n\
+3) Country\n\
+4) IP\n\
+		");
+
+	const parse = cli.parseInt(ans)
+	if(parse === false || parse === null) {
+		cli.quit();
+		return;
+	}
+
+	switch(parse) {
+		case 1:
+		break;
+		case 2:
+		const ans22 = cli.questionSync("Choose a Continent?");
+		break;
+		case 3:
+		const ans23 = cli.questionSync("Choose a Country?");
+		break;
+		case 4:
+		const ans24 = cli.questionSync("Please state IP");
+		break;
+	}
+
 }
